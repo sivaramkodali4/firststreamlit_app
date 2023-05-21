5a00fbe4143251097408b94dd87fb808329231b9
sivaramkodali4 <134171274+sivaramkodali4@users.noreply.github.com>
2023-05-21 10:35:34 -0400

    Update streamlit_app.py
    Add an additional comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title('My Pareants New Healthy Diner')
-
-streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
